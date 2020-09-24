5dcc2a25b6abe6383f828db66fb1ff4d382107d9
G Keerthi <g55@dxc.com>
2020-09-24 15:54:22 +0530

    Added 3rd line

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -8,6 +8,7 @@ namespace ConsoleApp1
         {
             Console.WriteLine("Hello World!");
             Console.WriteLine("Welcome to Git");
+            Console.WriteLine("Git maintains version History");
         }
     }
 }
